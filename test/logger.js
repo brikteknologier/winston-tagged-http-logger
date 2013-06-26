df7407a7e87f0a5b623453a2b9b486f62f5cef6e
Jon Packer <contact@jonpacker.com>
2013-06-26 19:17:24 +0200

    Added vanilla node server tests

---
+++ test/logger.js
@@ -0,0 +1,68 @@
+var http = require('http');
+var wthl = require('../');
+var winston = require('winston');
+var TaggedLogger = require('tagged-logger');
+var assert = require('assert');
+var express = require('express');
+var request = require('supertest');
+
+describe('Tagged http logger', function() {
+  var logger;
+  var transport;
+  beforeEach(function() {
+    transport = new winston.transports.Memory();
+    var baseLogger = new winston.Logger({transports: [transport]});
+    logger = new TaggedLogger(baseLogger, []);
+  });
+  
+  describe('Vanilla HTTP server', function() {
+    var server;
+    beforeEach(function() {
+      server = http.createServer(function(req, res) {
+        if (req.url == '/sync') {
+          res.end();
+        } else if (req.url == '/async') {
+          setTimeout(function() { res.statusCode = 201; res.end() }, 200);
+        }
+      });
+      wthl(server, logger);
+    });
+
+    it('should log the http listen event', function(done) {
+      server.listen(0, function() {
+        process.nextTick(function() {
+          assert(transport.writeOutput.length == 1);
+          assert(transport.writeOutput[0].match(/listening/gi));
+          done();
+        });
+      });
+    });
+
+    it('should log a request', function(done) {
+      server.listen(0, function() {
+        request(server).get('/sync').end(function(err, res) {
+          setTimeout(function() {
+            assert(transport.writeOutput.length == 2);
+            assert(transport.writeOutput[1].match(/get/gi));
+            assert(transport.writeOutput[1].match(/200/g));
+            done();
+          }, 50);
+        });
+      });
+    });
+
+    it('should log an async request', function(done) {
+      server.listen(0, function() {
+        request(server).get('/async').end(function(err, res) {
+          setTimeout(function() {
+            assert(transport.writeOutput.length == 2);
+            assert(transport.writeOutput[1].match(/get/gi));
+            assert(transport.writeOutput[1].match(/201/g));
+            done();
+          }, 50);
+        });
+      });
+    });
+  });
+});
+
